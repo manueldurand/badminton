66f9aa10f074384c2a7c29efafeae7f22c419db0
manuel_durand <manuel.durand@mailo.com>
2023-01-11 18:30:01 +0100

    tp ok

--- common/footer.php
+++ common/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer text-faded text-center py-5">
-        <div class="container">
-            <p class="m-0 small">Copyright &copy; Manuel Durand 2023</p>
-        </div>
-    </footer>
+    <div class="container">
+        <p class="m-0 small">Copyright &copy; Manuel Durand 2023</p>
+    </div>
+</footer>
